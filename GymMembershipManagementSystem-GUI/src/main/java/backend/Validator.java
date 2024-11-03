e52c9784500f92159e00939da92f38ac1486a980
Youssef-Keshk <160512220+Youssef-Keshk@users.noreply.github.com>
2024-11-03 16:13:41 +0200

    Added methods to validate Membership(type and status)

--- GymMembershipManagementSystem-GUI/src/main/java/backend/Validator.java
+++ GymMembershipManagementSystem-GUI/src/main/java/backend/Validator.java
@@ -28,8 +28,20 @@ public class Validator {
 
     // Method to validate phone number --> (+20 0123456789 OR +20123456789 OR 0123456789)
     public static boolean isValidPhoneNumber (String phoneNumber) {
-        String regex = "^(\\+20\\s(10|11|12|15)\\d{8}|\\+20(10|11|12|15)\\d{8}|0(10|11|12|15)\\d{8})$";
+        String regex = "^(\\+20\\s?(10|11|12|15)\\d{8}|0(10|11|12|15)\\d{8})$";
         return phoneNumber.matches(regex);
+    }
+    
+    // Method to validate membership type --> (monthly, yearly, premium)
+    public static boolean isValidMembershipType (String type) {
+        boolean flag = type.trim().equalsIgnoreCase("monthly") || type.trim().equalsIgnoreCase("yearly") || type.trim().equalsIgnoreCase("premium");
+        return flag;
+    }
+    
+    // Method to validate membership status --> (Active, Canceled)
+    public static boolean isValidMembershipStatus (String status) {
+        boolean flag = status.trim().equalsIgnoreCase("Active") || status.trim().equalsIgnoreCase("Canceled");
+        return flag;
     }
 
 }
